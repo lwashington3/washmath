ff85c61ced1de7f2ea138f132e0a261f9bb9681a
Len Washington <l.washingtoniii.27@gmail.com>
2022-04-15 20:57:52 -0500

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ git_url = "https://github.com/lwashington3/washmath"
 
 setup(
 	name=project_name,
-	version="1.0.2",
+	version="2.0.0",
 	author="Len Washington III",
 	author_email="l.washingtoniii.27@gmail.com",
 	description="Custom mathematics module",
