aac6c35d3f449dd45389f773665f4db9f625a402
Len Washington <l.washingtoniii.27@gmail.com>
2022-06-12 16:12:44 -0500

    Edited euler's function to accept the "until" argument

--- washmath/__main__.py
+++ washmath/__main__.py
@@ -1,6 +1,4 @@
 from argparse import ArgumentParser
-from os import getenv
-from sys import argv
 
 
 def _add_common_options(parser):
@@ -12,6 +10,7 @@ def _create_euler_parser(parser_factory):
 	_add_common_options(parser)
 	parser.add_argument("-g", "--step_size", help="The step size. (h is used for help)")
 	parser.add_argument("-n", "--steps", help="The number of steps to go (inclusive).")
+	parser.add_argument("-u", "--until", help="The highest value you want to find")
 	parser.add_argument("-x", "--initial_x", help="The initial x-value.")
 	parser.add_argument("-y", "--initial_y", help="The initial y-value.")
 	parser.add_argument("-f", "--function", help="The python equivalent of a lambda function for the differential equation.")
@@ -31,16 +30,24 @@ def main(args):
 	parser = create_argument_parser()
 	arguments = parser.parse_args(args)
 	if arguments.command == "euler":
+		if not arguments.steps and not arguments.until:
+			raise ValueError("The euler method needs to know the number of steps to takes (n) or when to stop (u).")
 		from .calc import euler
 		from tabulate import tabulate
-		h = float(arguments.step_size)
-		n = int(arguments.steps) + 1
-		x0 = float(arguments.initial_x)
-		y0 = float(arguments.initial_y)
-		function = eval(arguments.function)
-		answers = euler(h, n, x0, y0,  function, answer_only=False)
-		print(tabulate(zip(*answers), headers=("x", "y"), tablefmt="fancy_grid"))
+
+		kwargs = {
+			"h": float(arguments.step_size),
+			"n": int(arguments.steps) + 1 if arguments.steps is not None else None,
+			"until": int(arguments.until) if arguments.until is not None else None,
+			"x0": float(arguments.initial_x),
+			"y0": float(arguments.initial_y),
+			"function": eval(arguments.function),
+			"answer_only": False
+		}
+		answers = euler(**kwargs)
+		print(tabulate(zip(*answers), headers=("Indexes", "x", "y"), tablefmt="fancy_grid"))
 
 
 if __name__ == "__main__":
+	from sys import argv
 	main(argv[1:])
--- washmath/calc/differentials.py
+++ washmath/calc/differentials.py
@@ -2,25 +2,38 @@ from ..tools import Fraction
 from ..trig import E
 
 
-def euler(h:float, n:int, x0, y0, func, answer_only=True) -> Fraction | tuple[tuple[Fraction], tuple[Fraction]]:
+def check_conditions(lst, ys, steps, until) -> bool:
+	if steps is not None:
+		return len(lst) < steps - 1
+	return ys[-1] < until
+
+
+def euler(h:float, x0, y0, function, n:int=None, until=None, answer_only=True) -> Fraction | tuple[tuple[Fraction], tuple[Fraction]]:
 	"""
 	:param float|int h: The horizontal step size
 	:param int n: The number of steps to take
 	:param int|float x0: The initial value of the independent variable
 	:param int|float y0: The initial value of the dependent variable
-	:param function func: The function that is equivalent to the differential. Should take two arguments, the first being the independent variable, usually denoted as x, the second being the dependent variable, denoted as y
+	:param function: The function that is equivalent to the differential. Should take two arguments, the first being the independent variable, usually denoted as x, the second being the dependent variable, denoted as y
 	:param bool answer_only: If the method should only return the final y, or the arrays of all answers
 	"""
+	if n is None and until is None:
+		raise ValueError("Euler method needs number of steps (n) or until to define when the function is finished")
+
 	xs = [Fraction(x0)]
 	ys = [Fraction(y0)]
-	for i in range(1, n):
-		y = ys[-1] + (func(xs[-1], ys[-1]) * h)
+	indexes = [1]
+	while check_conditions(indexes, ys, n, until):
+		y = ys[-1] + (function(xs[-1], ys[-1]) * h)
 
 		xs.append(xs[-1] + h)
 		ys.append(y)
+		indexes.append(indexes[-1] + 1)
+
 	if answer_only:
 		return ys[-1]
-	return xs, ys
+
+	return indexes, xs, ys
 
 
 def P(t, p0, k, M) -> Fraction:
